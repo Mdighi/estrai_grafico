1c43922b2dce39879f754e5aba2ebc852353f390
Mdighi <165094162+Mdighi@users.noreply.github.com>
2025-07-10 01:30:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
+st.set_page_config(page_title="Seleziona Lingua / Select Language")  # deve essere prima di tutto
+
 import estrai_grafico
 import plot_extraction
-
-st.set_page_config(page_title="Seleziona Lingua / Select Language")
 
 st.markdown(
     "<div style='text-align: center;'>"
